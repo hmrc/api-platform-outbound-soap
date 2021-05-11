733c557b6f6ce4f314b976a90ac2e845c4ac0251
Neil Frow <675806+worthydolt@users.noreply.github.com>
2021-05-11 11:29:57 +0100

    APID-107: remove commented code

--- app/uk/gov/hmrc/apiplatformoutboundsoap/services/OutboundService.scala
+++ app/uk/gov/hmrc/apiplatformoutboundsoap/services/OutboundService.scala
@@ -174,14 +174,12 @@ class OutboundService @Inject()(outboundConnector: OutboundConnector,
   }
 
   private def addOptionalAddressingHeaders(message: MessageRequest, wsaNs: OMNamespace, envelope: SOAPEnvelope): Unit = {
-    //    message.addressing foreach { addressing =>
     message.addressing.from.foreach(addToSoapHeader(_, WSA_FROM, wsaNs, envelope))
     addToSoapHeader(message.addressing.to, WSA_TO, wsaNs, envelope)
     message.addressing.replyTo.foreach(addToSoapHeader(_, WSA_REPLY_TO, wsaNs, envelope))
     message.addressing.faultTo.foreach(addToSoapHeader(_, WSA_FAULT_TO, wsaNs, envelope))
     addToSoapHeader(message.addressing.messageId, WSA_MESSAGE_ID, wsaNs, envelope)
     message.addressing.relatesTo.foreach(addToSoapHeader(_, WSA_RELATES_TO, wsaNs, envelope))
-    //    }
   }
 
   private def addToSoapHeader(property: String, elementName: String, namespace: OMNamespace, envelope: SOAPEnvelope): Unit = {
