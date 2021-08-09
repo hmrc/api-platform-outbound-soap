3eaf647045405658e340c777e3d660a32276c0f6
Neil Frow <675806+worthydolt@users.noreply.github.com>
2021-08-09 16:42:07 +0100

    APID-233: updated to use Play 2.8 with required parts of retired scheduling library copy/pasted

--- app/uk/gov/hmrc/apiplatformoutboundsoap/scheduled/SchedulerModule.scala
+++ app/uk/gov/hmrc/apiplatformoutboundsoap/scheduled/SchedulerModule.scala
@@ -17,14 +17,13 @@
 package uk.gov.hmrc.apiplatformoutboundsoap.scheduled
 
 import com.google.inject.AbstractModule
-import play.api.{Application, Configuration, Environment}
+import javax.inject.{Inject, Provider, Singleton}
 import play.api.inject.{ApplicationLifecycle, Binding, Module}
+import play.api.{Application, Configuration, Environment}
 import play.modules.reactivemongo.ReactiveMongoComponent
 import uk.gov.hmrc.apiplatformoutboundsoap.config.AppConfig
 import uk.gov.hmrc.lock.LockRepository
-import uk.gov.hmrc.play.scheduling.{RunningOfScheduledJobs, ScheduledJob}
 
-import javax.inject.{Inject, Provider, Singleton}
 import scala.concurrent.ExecutionContext
 
 class SchedulerModule extends AbstractModule {
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,15 +6,19 @@ object AppDependencies {
   val akkaVersion = "2.6.14"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-27" % "3.0.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-27" % "0.50.0",
-    "uk.gov.hmrc" %% "play-scheduling-play-27" % "7.10.0",
+    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.9.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.52.0",
+//    "uk.gov.hmrc" %% "play-scheduling-play-27" % "7.10.0",
+    "uk.gov.hmrc" %% "mongo-lock" % "7.0.0-play-28",
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "8.0.0-play-28",
     "com.typesafe.play" %% "play-json-joda" % "2.9.2",
     "com.lightbend.akka" %% "akka-stream-alpakka-mongodb" % "3.0.1",
     "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
     "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+    "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion,
     "com.typesafe.akka" %% "akka-stream" % akkaVersion,
     "com.typesafe.akka" %% "akka-protobuf-v3" % akkaVersion,
+    "com.typesafe.akka" %% "akka-serialization-jackson" % akkaVersion,
     "org.reactivemongo" %% "reactivemongo-akkastream" % "0.20.13",
     "org.apache.axis2" % "axis2-kernel" % "1.7.9",
     "org.apache.wss4j" % "wss4j-ws-security-dom" % "2.3.0",
@@ -24,15 +28,15 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-27" % "3.0.0" % "test, it",
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.9.0" % "test, it",
     "org.scalatest" %% "scalatest" % "3.1.2" % "test, it",
     "org.mockito" %% "mockito-scala-scalatest" % "1.14.4" % "test, it",
     "com.typesafe.play" %% "play-test" % current % "test, it",
     "com.vladsch.flexmark" %  "flexmark-all" % "0.35.10" % "test, it",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3" % "test, it",
+    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % "test, it",
     "com.github.tomakehurst" % "wiremock" % "2.25.1" % "it",
     "org.xmlunit" % "xmlunit-core" % "2.8.1" % "test, it",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-27" % "0.50.0" % "it"
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % "0.52.0" % "it"
   )
 
   val jettyVersion = "9.2.24.v20180105"
