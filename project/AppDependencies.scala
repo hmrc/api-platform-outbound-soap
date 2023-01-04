b7567831bd370e8b4e20443d45e000d4afd4fa45
John Green <john.green@digital.hmrc.gov.uk>
2023-01-04 12:41:41 +0000

    API-5546: Upgrade play bootstrap + related updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,29 +3,29 @@ import play.sbt.PlayImport.caffeine
 import sbt._
 
 object AppDependencies {
-  val bootstrapPlayVersion = "7.8.0"
-  val mongoVersion = "0.73.0"
+  val bootstrapPlayVersion = "7.12.0"
+  val mongoVersion         = "0.74.0"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % bootstrapPlayVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % mongoVersion,
-    "com.typesafe.play" %% "play-json-joda" % "2.9.2",
+    "uk.gov.hmrc"        %% "bootstrap-backend-play-28"   % bootstrapPlayVersion,
+    "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-28"          % mongoVersion,
+    "com.typesafe.play"  %% "play-json-joda"              % "2.9.2",
     "com.lightbend.akka" %% "akka-stream-alpakka-mongodb" % "3.0.4",
-    "org.apache.axis2" % "axis2-kernel" % "1.8.0",
-    "org.apache.wss4j" % "wss4j-ws-security-dom" % "2.4.1",
-    "com.beachape" %% "enumeratum-play-json" % "1.7.0",
+    "org.apache.axis2"   %  "axis2-kernel"                % "1.8.0",
+    "org.apache.wss4j"   %  "wss4j-ws-security-dom"       % "2.4.1",
+    "com.beachape"       %% "enumeratum-play-json"        % "1.7.0",
     caffeine
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapPlayVersion % "test, it",
-    "org.scalatest" %% "scalatest" % "3.2.12" % "test, it",
-    "org.mockito" %% "mockito-scala-scalatest" % "1.17.7" % "test, it",
-    "com.typesafe.play" %% "play-test" % current % "test, it",
-    "com.vladsch.flexmark" %  "flexmark-all" % "0.64.0" % "test, it",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % "test, it",
-    "org.xmlunit" % "xmlunit-core" % "2.9.0" % "test, it",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % mongoVersion % "it"
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapPlayVersion % "test, it",
+    "org.scalatest"          %% "scalatest"               % "3.2.12"             % "test, it",
+    "org.mockito"            %% "mockito-scala-scalatest" % "1.17.7"             % "test, it",
+    "com.typesafe.play"      %% "play-test"               % current              % "test, it",
+    "com.vladsch.flexmark"   %  "flexmark-all"            % "0.64.0"             % "test, it",
+    "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"              % "test, it",
+    "org.xmlunit"            %  "xmlunit-core"            % "2.9.0"              % "test, it",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % mongoVersion         % "it"
   )
 
   val axiomVersion = "1.4.0"
